a203198319da971e84749d234b8e876fa9969319
Tilvadami <1021995543@qq.com>
2024-07-30 20:05:11 +0800

    计算完hrv

--- demo/test.py
+++ demo/test.py
@@ -3,22 +3,52 @@
 
 import numpy as np
 import os
+import scipy.io as scio
+import neurokit2 as nk
 
-data_root = r'../DE'
+# data_root = r'../DE'
+#
+# filelist = os.listdir(data_root)
+# print(filelist)
+#
+# demo_pth_ECG = r'../DE/1-DE-ECG.npy'
+# demo_pth_EEG = r'../DE/1-DE-EEG.npy'
+# data_ECG = np.load(demo_pth_ECG)
+# data_EEG = np.load(demo_pth_EEG)
+# print('=============ECG=============')
+# print(type(data_ECG))
+# print(data_ECG.shape)
+# # print(data_ECG)
+# print('=============EEG=============')
+# print(type(data_EEG))
+# print(data_EEG.shape)
+# # print(data_EEG)
 
-filelist = os.listdir(data_root)
-print(filelist)
+# ====================================
 
-demo_pth_ECG = r'../DE/1-DE-ECG.npy'
-demo_pth_EEG = r'../DE/1-DE-EEG.npy'
-data_ECG = np.load(demo_pth_ECG)
-data_EEG = np.load(demo_pth_EEG)
-print('=============ECG=============')
-print(type(data_ECG))
-print(data_ECG.shape)
-# print(data_ECG)
-print('=============EEG=============')
-print(type(data_EEG))
-print(data_EEG.shape)
-# print(data_EEG)
+dir_root = r'D:\pythonPROJ\secondPaperData\ECG'
+filelist = os.listdir(dir_root)
+numEpochs = 24
+for filename in filelist:
+    filePath = os.path.join(dir_root, filename)
+
+    data_ecg = scio.loadmat(filePath)['data'].transpose(2, 1, 0)
+    print(data_ecg.shape)
+
+    # hrv_arr = np.zeros((numEpochs, 9))  # 9个指标
+    hrv_arr = []
+    for i in range(numEpochs):
+        singelEpoch = -data_ecg[i].flatten()
+        print(singelEpoch.shape)
+        peaks, info = nk.ecg_peaks(singelEpoch, sampling_rate=250)
+        hrv_indices = nk.hrv(peaks, sampling_rate=250)
+        spec_columns = hrv_indices[['HRV_MeanNN', 'HRV_SDNN', 'HRV_SDANN1', 'HRV_RMSSD', 'HRV_pNN50', 'HRV_HF',
+                                    'HRV_LF', 'HRV_VLF', 'HRV_TP']].to_numpy()
+        print('spec_columns.shape:', spec_columns.shape)
+        hrv_arr.append(spec_columns)
+    hrv_arr = np.array(hrv_arr).reshape((24, 9))
+    print('hrv_arr.shape:', hrv_arr.shape)
+    name = filename.split('_')[1]
+    np.save(f'../data/ecg_hrv/T_{name}_hrv.npy', hrv_arr)
+
 
